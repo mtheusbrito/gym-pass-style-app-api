5dd0ead8d89b132474d7ec24b44c935a1765c69c
mtheusbrito <matheusbritodasilva@gmail.com>
2023-07-07 01:38:09 -0300

    Primeira migration com Prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
+const prisma = new PrismaClient()
 export const app = fastify()
+prisma.user.create({
+  data: {
+    name: 'Test',
+    email: 'email@gmail.com',
+  },
+})
