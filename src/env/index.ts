117d13e16ead736d2d5dbcbe82fb533385961a04
mtheusbrito <matheusbritodasilva@gmail.com>
2023-07-06 22:08:21 -0300

    Carregando variáveis ambiente

---
+++ src/env/index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+
+import z from 'zod'
+const envSchemma = z.object({
+    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+    PORT: z.coerce.number().default(3333)
+})
+
+const _env = envSchemma.safeParse(process.env)
+
+if(!_env.success){
+    console.log('❌ Invalid environment variables', _env.error.format())
+
+    throw new Error('Invalid environment variables')
+}
+
+export const env = _env.data
