abfeac9367236483a3010e34b0ecd90d71fa9c22
mtheusbrito <matheusbritodasilva@gmail.com>
2023-07-10 13:49:26 -0300

    Controller de perfil

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,6 +2,7 @@ import { authenticate } from './controllers/authenticate'
 import { profile } from './controllers/profile'
 import { register } from './controllers/register'
 import { FastifyInstance } from 'fastify'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
@@ -9,5 +10,6 @@ export async function appRoutes(app: FastifyInstance) {
   app.post('/sessions', authenticate)
 
   // Authenticated
-  app.get('/me', profile)
+
+  app.get('/me', { onRequest: verifyJWT }, profile)
 }
