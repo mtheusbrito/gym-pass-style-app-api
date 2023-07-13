0992182c96b4a67a25ddea5dd984943b5e74cc5e
mtheusbrito <matheusbritodasilva@gmail.com>
2023-07-12 21:26:56 -0300

    Autorização por cargos (RBAC)

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -4,11 +4,16 @@ import { create } from './create'
 import { history } from './history'
 import { metrics } from './metrics'
 import { validate } from './validate'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
   app.post('/', create)
   app.get('/history', history)
   app.get('/metrics', metrics)
-  app.patch('/:checkInId/validate', validate)
+  app.patch(
+    '/:checkInId/validate',
+    { onRequest: [verifyUserRole('ADMIN')] },
+    validate,
+  )
 }
